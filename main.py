3cc5fcd93cf3ac8fc48fa36c73a57ee0e7086d34
shirley <shirley.chavez@uned.cr>
2023-04-24 20:12:18 -0600

    Initial commit for the first tim

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import random
 
-# Función para mostrar la ventana de verificar vocales
+# Clase para mostrar la ventana de verificar vocales
 class VerificacionesDecorator:
     def __init__(self, funcion_original):
         self.funcion_original = funcion_original
@@ -12,6 +12,18 @@ class VerificacionesDecorator:
         return self.funcion_original(*args, **kwargs)
 verificador = VerificacionesDecorator(None)
 cantidad_verificaciones = 0
+
+def guardar_verificaciones(verificaciones):
+    with open('verificaciones.txt', 'w') as file:
+        file.write(str(verificaciones))
+
+def leer_verificaciones():
+    try:
+        with open('verificaciones.txt', 'r') as file:
+            verificaciones = int(file.read())
+    except FileNotFoundError:
+        verificaciones = 1
+    return verificaciones
 
 
 def mostrar_verificar_vocales():
@@ -59,6 +71,7 @@ def mostrar_verificar_vocales():
     resultado2.pack()
 
     verificador = VerificacionesDecorator(verificar)
+    verificador.verificaciones = leer_verificaciones()
 
 
 # Función para mostrar la ventana de contar palabras
